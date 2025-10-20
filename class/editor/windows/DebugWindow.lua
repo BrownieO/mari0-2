39f3471f8f20516574c12be54efb0804cd8c27dc
BrownieO <asdsadasd@asdffds.fcd>
2025-10-20 05:51:48 -0300

    Add basic death logic

--- class/editor/windows/DebugWindow.lua
+++ class/editor/windows/DebugWindow.lua
@@ -6,8 +6,9 @@ DebugWindow.width = 8*17+21
 DebugWindow.height = 200
 
 
-local function changeTemplate(template)
-    game.players[1].actor:loadActorTemplate(template)
+local function changeTemplate(powerUp)
+    game.players[1].actor:loadActorTemplate(actorTemplates["smb3_" .. powerUp])
+	game.players[1].powerUp = powerUp
 end
 
 function DebugWindow:initialize(editor)
@@ -24,13 +25,13 @@ function DebugWindow:initialize(editor)
     self.editor.canvas:addChild(self.element)
 
     self.element:addChild(Gui3.Text:new("powerup state", 0, 0))
-    self.element:addChild(Gui3.TextButton:new(0, 0, "small", true, 0, function(button) changeTemplate(actorTemplates.smb3_small) end))
-    self.element:addChild(Gui3.TextButton:new(0, 0, "fire", true, 0, function(button) changeTemplate(actorTemplates.smb3_fire) end))
-    self.element:addChild(Gui3.TextButton:new(0, 0, "hammer", true, 0, function(button) changeTemplate(actorTemplates.smb3_hammer) end))
-    self.element:addChild(Gui3.TextButton:new(0, 0, "big", true, 0, function(button) changeTemplate(actorTemplates.smb3_big) end))
-    self.element:addChild(Gui3.TextButton:new(0, 0, "raccoon", true, 0, function(button) changeTemplate(actorTemplates.smb3_raccoon) end))
-    self.element:addChild(Gui3.TextButton:new(0, 0, "tanooki", true, 0, function(button) changeTemplate(actorTemplates.smb3_tanooki) end))
-    self.element:addChild(Gui3.TextButton:new(0, 0, "frog", true, 0, function(button) changeTemplate(actorTemplates.smb3_frog) end))
+    self.element:addChild(Gui3.TextButton:new(0, 0, "small", true, 0, function(button) changeTemplate("small") end))
+    self.element:addChild(Gui3.TextButton:new(0, 0, "fire", true, 0, function(button) changeTemplate("fire") end))
+    self.element:addChild(Gui3.TextButton:new(0, 0, "hammer", true, 0, function(button) changeTemplate("hammer") end))
+    self.element:addChild(Gui3.TextButton:new(0, 0, "big", true, 0, function(button) changeTemplate("big") end))
+    self.element:addChild(Gui3.TextButton:new(0, 0, "raccoon", true, 0, function(button) changeTemplate("raccoon") end))
+    self.element:addChild(Gui3.TextButton:new(0, 0, "tanooki", true, 0, function(button) changeTemplate("tanooki") end))
+    self.element:addChild(Gui3.TextButton:new(0, 0, "frog", true, 0, function(button) changeTemplate("frog") end))
 
     self.element.autoArrangeChildren = true
     self.element:sizeChanged()
