d6f5eb58ab1c878c6011f956989dbca45c1556d2
adasdsad <asdsadasd@asdffds.fcd>
2025-10-20 02:55:17 -0300

    Add power-up actors

---
+++ actorTemplates/mushroom.lua
@@ -0,0 +1,15 @@
+return {
+    width = 16,
+    height = 16,
+
+    img = "img/actors/mushroom.png",
+    quadWidth = 16,
+    quadHeight = 16,
+    centerX = 8,
+    centerY = 8,
+
+    components = {
+        ["misc.unrotate"] = {},
+		["misc.removeOnMarioContact"] = {},
+    }
+}
