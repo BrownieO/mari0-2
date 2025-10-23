4df15ec249da566ee5ed5edbbdbea65073011882
BrownieO <187204825+BrownieO@users.noreply.github.com>
2025-10-23 02:27:08 -0300

    Add basic sound support

--- componentLoader.lua
+++ componentLoader.lua
@@ -1,16 +1,15 @@
 components = {}
 
-toLoad = recursiveEnumerate("components")
+local toLoad = recursiveEnumerate("components")
 
 for _, path in ipairs(toLoad) do
     if path:sub(-3) == "lua" then
         -- clean up the string
-
         path = path:gsub("/", ".") -- replace / with .
         path = path:sub(1, -5) -- remove .lua
-        name = path:sub(12) -- really?
+        name = path:sub(12) -- remove folder name ("components/"). lol hardcode
 
-        components[name] = require(path) -- sandbox this?
+        components[name] = require(path) -- FIXME: sandbox this!!!1
 
         assert(type(components[name]) == "table", string.format("Component \"%s\" didn't return a table.", name))
     end
