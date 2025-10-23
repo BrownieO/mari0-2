b46282e5f87dff924c48145fc3a788e3278353e6
BrownieO <187204825+BrownieO@users.noreply.github.com>
2025-10-23 20:52:54 -0300

    Switch collision resolve directions
    whywhywhywhywhywhywhy

--- components/misc/removeOnMarioContact.lua
+++ components/misc/removeOnMarioContact.lua
@@ -6,19 +6,19 @@ function removeOnMarioContact:initialize(actor, args)
 end
 
 function removeOnMarioContact:leftCollision(dt, actorEvent, obj2)
-    self:resolve("left", obj2)
-end
-
-function removeOnMarioContact:rightCollision(dt, actorEvent, obj2)
     self:resolve("right", obj2)
 end
 
+function removeOnMarioContact:rightCollision(dt, actorEvent, obj2)
+    self:resolve("left", obj2)
+end
+
 function removeOnMarioContact:topCollision(dt, actorEvent, obj2)
-    self:resolve("top", obj2)
+    self:resolve("bottom", obj2)
 end
 
 function removeOnMarioContact:bottomCollision(dt, actorEvent, obj2)
-    self:resolve("bottom", obj2)
+    self:resolve("top", obj2)
 end
 
 function removeOnMarioContact:resolve(dir, obj2)
