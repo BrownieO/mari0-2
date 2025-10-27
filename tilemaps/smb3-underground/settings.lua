d5494168e299fb77a525cb2c9882e09a2ec83b39
BrownieO <187204825+BrownieO@users.noreply.github.com>
2025-10-27 02:44:26 -0300

    Update underground tileset collisions

--- tilemaps/smb3-underground/settings.lua
+++ tilemaps/smb3-underground/settings.lua
@@ -1,45 +1,15 @@
+--- Underground tileset
 local tiles = {}
 
 local templates = VAR("tileTemplates")
 
---collisions
-local cubes = {1, 2, 3, 9, 10, 11}
-
-for _, v in ipairs(cubes) do
-    tiles[v] = {collision = templates.cube}
+for i=1,56 do
+    tiles[i] = {collision = templates.cube}
 end
+tiles[19] = nil
+tiles[36] = nil
 
--- top colliding things
-local topThings = {7, 17, 18, 19, 20, 21, 22, 41, 42, 43, 44, 45, 46}
-
-for _, v in ipairs(topThings) do
-    tiles[v] = {
-        collision = templates.cube,
-        exclusiveCollision = 1,
-    }
-end
-
-local stampMaps = {
-    {
-        name = "ground",
-        type = "quads",
-        map = {
-            {1, 2, 3},
-            {9, 10, 11},
-        },
-        paddings = {1, 1, 0, 1}, -- clockwise from top (like in css)
-    },
-
-    {
-        name = "cloud",
-        type = "quads",
-        map = {
-            {4, 5, 6},
-            {12, 13, 14},
-        },
-        paddings = {2, 1, 0, 1}, -- clockwise from top (like in css)
-    }
-}
+local stampMaps = {}
 
 local props = {
     tileSize = 16,
