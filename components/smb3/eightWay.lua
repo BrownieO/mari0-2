8e47b60c36025156c8d414cd1d781559437867b8
BrownieO <187204825+BrownieO@users.noreply.github.com>
2025-11-07 18:18:56 -0300

    Update Balloon Mario

--- components/smb3/eightWay.lua
+++ components/smb3/eightWay.lua
@@ -2,7 +2,9 @@ local Component = require "class.Component"
 local eightWay = class("category.eightWay", Component)
 
 local ACCELERATION = 150
+local DECCELERATION = ACCELERATION/2
 local MAX_SPEED = 60
+local RUN_MAX_SPEED = 120
 
 function eightWay:initialize(actor, args)
     Component.initialize(self, actor, args)
@@ -10,27 +12,37 @@ function eightWay:initialize(actor, args)
 	self.actor.gravity = 0
 end
 
-local function accelerate(dt, actor, acceleration, maxSpeed)
-	if controls3.cmdDown("left") and not controls3.cmdDown("right") then
-		actor.speed[1] = math.max(-maxSpeed, actor.speed[1] - acceleration*dt)
+local maxSpeed
+local function accelerate(dt, actor, acceleration)
+	if controls3.cmdDown("run") then
+		maxSpeed = RUN_MAX_SPEED
+	else
+		maxSpeed = MAX_SPEED
 	end
 
 	if controls3.cmdDown("right") and not controls3.cmdDown("left") then
 		actor.speed[1] = math.min(maxSpeed, actor.speed[1] + acceleration*dt)
+	elseif controls3.cmdDown("left") and not controls3.cmdDown("right") then
+		actor.speed[1] = math.max(-maxSpeed, actor.speed[1] - acceleration*dt)
+	elseif actor.speed[1] > 0 then
+		actor.speed[1] = actor.speed[1] - DECCELERATION*dt
+	elseif actor.speed[1] < 0 then
+		actor.speed[1] = actor.speed[1] + DECCELERATION*dt
 	end
 
 	if controls3.cmdDown("up") and not controls3.cmdDown("down") then
 		actor.speed[2] = math.max(-maxSpeed, actor.speed[2] - acceleration*dt)
-	end
-
-	if controls3.cmdDown("down") and not controls3.cmdDown("up") then
+	elseif controls3.cmdDown("down") and not controls3.cmdDown("up") then
 		actor.speed[2] = math.min(maxSpeed, actor.speed[2] + acceleration*dt)
+	elseif actor.speed[2] > 0 then
+		actor.speed[2] = actor.speed[2] - DECCELERATION*dt
+	elseif actor.speed[2] < 0 then
+		actor.speed[2] = actor.speed[2] + DECCELERATION*dt
 	end
-
 end
 
 function eightWay:update(dt, actorEvent)
-	accelerate(dt, self.actor, ACCELERATION, MAX_SPEED)
+	accelerate(dt, self.actor, ACCELERATION)
 end
 
 return eightWay
