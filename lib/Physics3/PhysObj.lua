04d1df9294d6429924989ceb31e43716a8118a1d
BrownieO <187204825+BrownieO@users.noreply.github.com>
2025-11-11 02:44:54 -0300

    Fix indentation

--- lib/Physics3/PhysObj.lua
+++ lib/Physics3/PhysObj.lua
@@ -2,338 +2,338 @@ local Vector = require "lib.Vector"
 local PhysObj = class("Physics3.PhysObj")
 
 function PhysObj:initialize(world, x, y, width, height)
-	self.x = x
-	self.y = y
-	self.width = width
-	self.height = height
-	self.world = world
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.world = world
 
-	self.speed = {0, 0}
+    self.speed = {0, 0}
 
-	self.gravityDirection = math.pi*.5
+    self.gravityDirection = math.pi * .5
 
-	self.angle = 0
+    self.angle = 0
 
     self.surfaceAngle = 0
-	self.onGround = true
+    self.onGround = true
 
-	self.isGroundFor = {}
-	self.inPortals = {}
+    self.isGroundFor = {}
+    self.inPortals = {}
 
-	-- register yourself with the world
-	world:addObject(self)
+    -- register yourself with the world
+    world:addObject(self)
 
-	self:createTracers()
+    self:createTracers()
 end
 
 function PhysObj:createTracers()
-	-- More tracers than a quickplay match in Overwatch
-	self.tracers = {}
-	self.tracers.left = {}
-	self.tracers.right = {}
-	self.tracers.up = {}
-	self.tracers.down = {}
+    -- More tracers than a quickplay match in Overwatch
+    self.tracers = {}
+    self.tracers.left = {}
+    self.tracers.right = {}
+    self.tracers.up = {}
+    self.tracers.down = {}
 
-	local step = self.world.tileSize
+    local step = self.world.tileSize
 
-	-- Create left tracers
-	local xOff = math.floor(self.width/2)-1
-	local distance = math.floor(self.width/2)
+    -- Create left tracers
+    local xOff = math.floor(self.width / 2) - 1
+    local distance = math.floor(self.width / 2)
 
-	for yOff = self.height - Physics3.TRACER_BOTTOM_DIST - 1, Physics3.TRACER_SIDE_TOP_DIST+1, -step do
-		table.insert(self.tracers.left, Physics3.Tracer:new(self, xOff, yOff, Vector(-distance, 0)))
-	end
+    for yOff = self.height - Physics3.TRACER_BOTTOM_DIST - 1, Physics3.TRACER_SIDE_TOP_DIST + 1, -step do
+        table.insert(self.tracers.left, Physics3.Tracer:new(self, xOff, yOff, Vector(-distance, 0)))
+    end
 
-	-- Also include almost top
-	table.insert(self.tracers.left, Physics3.Tracer:new(self, xOff, Physics3.TRACER_SIDE_TOP_DIST, Vector(-distance, 0)))
+    -- Also include almost top
+    table.insert(
+        self.tracers.left,
+        Physics3.Tracer:new(self, xOff, Physics3.TRACER_SIDE_TOP_DIST, Vector(-distance, 0))
+    )
 
+    -- Create right tracers
+    xOff = math.floor(self.width / 2)
+    distance = math.floor(self.width / 2)
 
-	-- Create right tracers
-	xOff = math.floor(self.width/2)
-	distance = math.floor(self.width/2)
+    for yOff = self.height - Physics3.TRACER_BOTTOM_DIST - 1, Physics3.TRACER_SIDE_TOP_DIST + 1, -step do
+        table.insert(self.tracers.right, Physics3.Tracer:new(self, xOff, yOff, Vector(distance, 0)))
+    end
 
-	for yOff = self.height - Physics3.TRACER_BOTTOM_DIST - 1, Physics3.TRACER_SIDE_TOP_DIST+1, -step do
-		table.insert(self.tracers.right, Physics3.Tracer:new(self, xOff, yOff, Vector(distance, 0)))
-	end
+    -- Also include almost top
+    table.insert(
+        self.tracers.right,
+        Physics3.Tracer:new(self, xOff, Physics3.TRACER_SIDE_TOP_DIST, Vector(distance, 0))
+    )
 
-	-- Also include almost top
-	table.insert(self.tracers.right, Physics3.Tracer:new(self, xOff, Physics3.TRACER_SIDE_TOP_DIST, Vector(distance, 0)))
+    -- Create bottom tracers
+    local yOff = self.height / 2
+    distance = self.height / 2 + Physics3.TRACER_BOTTOM_EXTEND
 
+    --from left side
+    for xOff = 0 + Physics3.TRACER_BOTTOM_SIDE_SPACING, math.floor(self.width / 2) - 1, Physics3.TRACER_BOTTOM_SPACING do
+        table.insert(self.tracers.down, Physics3.Tracer:new(self, xOff, yOff, Vector(0, distance)))
+    end
 
-	-- Create bottom tracers
-	local yOff = self.height/2
-	distance = self.height/2+Physics3.TRACER_BOTTOM_EXTEND
+    --from right side
+    for xOff = self.width - 1 - Physics3.TRACER_BOTTOM_SIDE_SPACING, math.floor(self.width / 2), -Physics3.TRACER_BOTTOM_SPACING do
+        table.insert(self.tracers.down, Physics3.Tracer:new(self, xOff, yOff, Vector(0, distance)))
+    end
 
-	--from left side
-	for xOff = 0+Physics3.TRACER_BOTTOM_SIDE_SPACING, math.floor(self.width/2)-1, Physics3.TRACER_BOTTOM_SPACING do
-		table.insert(self.tracers.down, Physics3.Tracer:new(self, xOff, yOff, Vector(0, distance)))
-	end
+    -- Create top tracers
+    yOff = self.height / 2 - 1
+    distance = self.height / 2
 
-	--from right side
-	for xOff = self.width-1-Physics3.TRACER_BOTTOM_SIDE_SPACING, math.floor(self.width/2), -Physics3.TRACER_BOTTOM_SPACING do
-		table.insert(self.tracers.down, Physics3.Tracer:new(self, xOff, yOff, Vector(0, distance)))
-	end
+    --from left side
+    for xOff = 0 + Physics3.TRACER_TOP_SPACING, math.floor(self.width / 2) - 1, step do
+        table.insert(self.tracers.up, Physics3.Tracer:new(self, xOff, yOff, Vector(0, -distance)))
+    end
 
-
-	-- Create top tracers
-	yOff = self.height/2-1
-	distance = self.height/2
-
-	--from left side
-	for xOff = 0+Physics3.TRACER_TOP_SPACING, math.floor(self.width/2)-1, step do
-		table.insert(self.tracers.up, Physics3.Tracer:new(self, xOff, yOff, Vector(0, -distance)))
-	end
-
-	--from right side
-	for xOff = self.width-1-Physics3.TRACER_TOP_SPACING, math.floor(self.width/2), -step do
-		table.insert(self.tracers.up, Physics3.Tracer:new(self, xOff, yOff, Vector(0, -distance)))
-	end
+    --from right side
+    for xOff = self.width - 1 - Physics3.TRACER_TOP_SPACING, math.floor(self.width / 2), -step do
+        table.insert(self.tracers.up, Physics3.Tracer:new(self, xOff, yOff, Vector(0, -distance)))
+    end
 end
 
 function PhysObj:changeSize(width, height)
-	local diffX = self.width-width
-	local diffY = self.height-height
+    local diffX = self.width - width
+    local diffY = self.height - height
 
-	self.x = self.x + diffX/2
-	self.y = self.y + diffY
+    self.x = self.x + diffX / 2
+    self.y = self.y + diffY
 
-	self.width = width
-	self.height = height
+    self.width = width
+    self.height = height
 
-	self:createTracers()
+    self:createTracers()
 end
 
 function PhysObj:unRotate(dt)
-	self.angle = normalizeAngle(self.angle)
+    self.angle = normalizeAngle(self.angle)
 
-	if self.angle > 0 then
-		self.angle = math.max(0, self.angle - VAR("rotationSpeed")*dt)
-	else
-		self.angle = math.min(0, self.angle + VAR("rotationSpeed")*dt)
-	end
+    if self.angle > 0 then
+        self.angle = math.max(0, self.angle - VAR("rotationSpeed") * dt)
+    else
+        self.angle = math.min(0, self.angle + VAR("rotationSpeed") * dt)
+    end
 end
 
 function PhysObj:leftColCheck()
-	local colX, colY, colObj
+    local colX, colY, colObj
 
-	for _, tracer in ipairs(self.tracers.left) do
-		local traceX, traceY, traceObj = tracer:trace()
+    for _, tracer in ipairs(self.tracers.left) do
+        local traceX, traceY, traceObj = tracer:trace()
 
-		if traceX and (not colX or traceX < colX) then
-			colX, colY, colObj = traceX, traceY, traceObj
-		end
-	end
+        if traceX and (not colX or traceX < colX) then
+            colX, colY, colObj = traceX, traceY, traceObj
+        end
+    end
 
-	return colX, colY, colObj
+    return colX, colY, colObj
 end
 
 function PhysObj:rightColCheck()
-	local colX, colY, colObj
+    local colX, colY, colObj
 
-	for _, tracer in ipairs(self.tracers.right) do
-		local traceX, traceY, traceObj = tracer:trace()
+    for _, tracer in ipairs(self.tracers.right) do
+        local traceX, traceY, traceObj = tracer:trace()
 
-		if traceX and (not colX or traceX > colX) then
-			colX, colY, colObj = traceX, traceY, traceObj
-		end
-	end
+        if traceX and (not colX or traceX > colX) then
+            colX, colY, colObj = traceX, traceY, traceObj
+        end
+    end
 
-	return colX, colY, colObj
+    return colX, colY, colObj
 end
 
 function PhysObj:topColCheck()
-	local colX, colY, colObj
+    local colX, colY, colObj
 
-	for _, tracer in ipairs(self.tracers.up) do
-		local traceX, traceY, traceObj = tracer:trace()
+    for _, tracer in ipairs(self.tracers.up) do
+        local traceX, traceY, traceObj = tracer:trace()
 
-		if traceX and (not colX or traceY < colY) then
-			colX, colY, colObj = traceX, traceY, traceObj
-		end
-	end
+        if traceX and (not colX or traceY < colY) then
+            colX, colY, colObj = traceX, traceY, traceObj
+        end
+    end
 
-	return colX, colY, colObj
+    return colX, colY, colObj
 end
 
 function PhysObj:bottomColCheck()
-	local colX, colY, colObj
+    local colX, colY, colObj
 
-	for _, tracer in ipairs(self.tracers.down) do
-		local traceX, traceY, traceObj = tracer:trace()
+    for _, tracer in ipairs(self.tracers.down) do
+        local traceX, traceY, traceObj = tracer:trace()
 
-		if traceX and (not colX or traceY < colY) then
-			colX, colY, colObj = traceX, traceY, traceObj
-		end
-	end
+        if traceX and (not colX or traceY < colY) then
+            colX, colY, colObj = traceX, traceY, traceObj
+        end
+    end
 
-	return colX, colY, colObj
+    return colX, colY, colObj
 end
 
 function PhysObj:leftColResolve(obj, x, y)
-	if not self:leftCollision(obj) then
-		if x then
-			if obj.class:isSubclassOf(PhysObj) then
-				self.x = obj.x+obj.width
-			else
-				self.x = x+1
-			end
-		end
+    if not self:leftCollision(obj) then
+        if x then
+            if obj.class:isSubclassOf(PhysObj) then
+                self.x = obj.x + obj.width
+            else
+                self.x = x + 1
+            end
+        end
 
-		self.speed[1] = math.max(self.speed[1], 0)
-	end
+        self.speed[1] = math.max(self.speed[1], 0)
+    end
 end
 
 function PhysObj:rightColResolve(obj, x, y)
-	if not self:rightCollision(obj) then
-		if x then
-			if obj.class:isSubclassOf(PhysObj) then
-				self.x = obj.x-self.width
-			else
-				self.x = x-self.width
-			end
-		end
+    if not self:rightCollision(obj) then
+        if x then
+            if obj.class:isSubclassOf(PhysObj) then
+                self.x = obj.x - self.width
+            else
+                self.x = x - self.width
+            end
+        end
 
-		self.speed[1] = math.min(self.speed[1], 0)
-	end
+        self.speed[1] = math.min(self.speed[1], 0)
+    end
 end
 
 function PhysObj:topColResolve(obj, x, y)
-	if not self:topCollision(obj) then
-		if y then
-			self.y = y+1
-		end
+    if not self:topCollision(obj) then
+        if y then
+            self.y = y + 1
+        end
 
-		self.speed[2] = math.max(self.speed[2], 0)
-	end
+        self.speed[2] = math.max(self.speed[2], 0)
+    end
 end
 
 function PhysObj:bottomColResolve(obj, x, y)
-	if not self:bottomCollision(obj) then
-		if not self.onGround then
-			self.onGround = true
-		end
+    if not self:bottomCollision(obj) then
+        if not self.onGround then
+            self.onGround = true
+        end
 
-		if y then
-			self.y = y-self.height
-		end
+        if y then
+            self.y = y - self.height
+        end
 
-		self.speed[2] = math.min(self.speed[2], 0)
+        self.speed[2] = math.min(self.speed[2], 0)
 
-		return true
-	end
+        return true
+    end
 
-	return false
+    return false
 end
 
 function PhysObj:checkCollisions()
-	return	self:leftColCheck() or
-			self:rightColCheck() or
-			self:bottomColCheck() or
-			self:topColCheck()
+    return self:leftColCheck() or self:rightColCheck() or self:bottomColCheck() or self:topColCheck()
 end
 
 function PhysObj:resolveCollisions()
-	if VAR("debug").tracerDebug then
-		for _, group in pairs(self.tracers) do
-			for _, tracer in ipairs(group) do
-				tracer.tracedLength = 0
-			end
-		end
-	end
+    if VAR("debug").tracerDebug then
+        for _, group in pairs(self.tracers) do
+            for _, tracer in ipairs(group) do
+                tracer.tracedLength = 0
+            end
+        end
+    end
 
-	-- update self.inPortals
-	clearTable(self.inPortals)
+    -- update self.inPortals
+    clearTable(self.inPortals)
 
-	for _, portal in ipairs(self.world.portals) do
-		if objectWithinPortalRange(portal, self.x+self.width/2, self.y+self.height/2) then
-			self.inPortals[portal] = true
-		end
-	end
+    for _, portal in ipairs(self.world.portals) do
+        if objectWithinPortalRange(portal, self.x + self.width / 2, self.y + self.height / 2) then
+            self.inPortals[portal] = true
+        end
+    end
 
-	local x, y, obj
+    local x, y, obj
 
-	if self.speed[1] <= 0 then
-		x, y, obj = self:leftColCheck()
-	end
+    if self.speed[1] <= 0 then
+        x, y, obj = self:leftColCheck()
+    end
 
-	if x then -- resolve the left collision
-		self:leftColResolve(obj, x, y)
-		obj:rightColResolve(self)
-	elseif self.speed[1] >= 0 then -- see if we got a right collision
-		x, y, obj = self:rightColCheck()
+    if x then -- resolve the left collision
+        self:leftColResolve(obj, x, y)
+        obj:rightColResolve(self)
+    elseif self.speed[1] >= 0 then -- see if we got a right collision
+        x, y, obj = self:rightColCheck()
 
-		if x then -- resolve the right collision
-			self:rightColResolve(obj, x, y)
-			obj:leftColResolve(self)
-		end
-	end
+        if x then -- resolve the right collision
+            self:rightColResolve(obj, x, y)
+            obj:leftColResolve(self)
+        end
+    end
 
-	x = nil
+    x = nil
 
-	if self.speed[2] >= 0 then
-		x, y, obj = self:bottomColCheck()
-	end
+    if self.speed[2] >= 0 then
+        x, y, obj = self:bottomColCheck()
+    end
 
-	if x then
-		if self.onGround or y <= self.y + self.height then
-			if self:bottomColResolve(obj, x, y) then
-				self.standingOn = obj
+    if x then
+        if self.onGround or y <= self.y + self.height then
+            if self:bottomColResolve(obj, x, y) then
+                self.standingOn = obj
 
-				if obj.class:isSubclassOf(PhysObj) then
-					obj:getStoodOn(self)
-				end
-			end
-			obj:topColResolve(self)
+                if obj.class:isSubclassOf(PhysObj) then
+                    obj:getStoodOn(self)
+                end
+            end
+            obj:topColResolve(self)
 
-			if type(obj) == "table" and obj:isInstanceOf(Physics3.Cell) then -- update the object's surfaceAngle
-				self.surfaceAngle = obj.tile.angle -- todo: May be wrong if colliding pixel is right underneath a slope's end!
-			else
-				self.surfaceAngle = 0
-			end
-		end
-	else
-		if self.onGround and self.speed[2] > 0 then -- start falling maybe
-			self:startFall()
-			self.onGround = false
-		end
+            if type(obj) == "table" and obj:isInstanceOf(Physics3.Cell) then -- update the object's surfaceAngle
+                self.surfaceAngle = obj.tile.angle -- todo: May be wrong if colliding pixel is right underneath a slope's end!
+            else
+                self.surfaceAngle = 0
+            end
+        end
+    else
+        if self.onGround and self.speed[2] > 0 then -- start falling maybe
+            self:startFall()
+            self.onGround = false
+        end
 
-		x, y, obj = self:topColCheck()
+        x, y, obj = self:topColCheck()
 
-		if x then -- resolve the right collision
-			self:topColResolve(obj, x, y)
+        if x then -- resolve the right collision
+            self:topColResolve(obj, x, y)
 
-			obj:bottomColResolve(self)
-		end
-	end
+            obj:bottomColResolve(self)
+        end
+    end
 end
 
 function PhysObj:preMovement()
-	self.standingOn = nil
-	iClearTable(self.isGroundFor)
+    self.standingOn = nil
+    iClearTable(self.isGroundFor)
 end
 
 function PhysObj:postMovement()
-	if self.standingOn and self.standingOn.class:isSubclassOf(PhysObj) then
-		local mx, my = recursivelyGetFrameMovement(self.standingOn)
+    if self.standingOn and self.standingOn.class:isSubclassOf(PhysObj) then
+        local mx, my = recursivelyGetFrameMovement(self.standingOn)
 
-		self.x = self.x + mx
-		self.y = self.y + my
-	end
+        self.x = self.x + mx
+        self.y = self.y + my
+    end
 end
 
 function PhysObj:checkCollision(x, y)
-	if pointInRectangle(x, y, math.round(self.x), math.round(self.y), self.width, self.height) then
-		return true
-	end
+    if pointInRectangle(x, y, math.round(self.x), math.round(self.y), self.width, self.height) then
+        return true
+    end
 end
 
 function PhysObj:portalled()
-	-- throw off anyone riding us
-	for _, obj in ipairs(self.isGroundFor) do
-		obj.standingOn = nil
-	end
+    -- throw off anyone riding us
+    for _, obj in ipairs(self.isGroundFor) do
+        obj.standingOn = nil
+    end
 
-	iClearTable(self.isGroundFor)
+    iClearTable(self.isGroundFor)
 end
 
 function recursivelyGetFrameMovement(obj, x, y) -- god I hate recursion
@@ -356,46 +356,51 @@ function recursivelyGetFrameMovement(obj, x, y) -- god I hate recursion
 end
 
 function PhysObj:getStoodOn(obj)
-	table.insert(self.isGroundFor, obj)
+    table.insert(self.isGroundFor, obj)
 end
 
 function PhysObj:debugDraw(xOff, yOff)
-	if not VAR("debug").tracerDebug then
-		love.graphics.setColor(1, 0, 0)
-		love.graphics.rectangle("line", self.x+.5, self.y+.5, self.width-1, self.height-1)
-	end
+    if not VAR("debug").tracerDebug then
+        love.graphics.setColor(1, 0, 0)
+        love.graphics.rectangle("line", self.x + .5, self.y + .5, self.width - 1, self.height - 1)
+    end
 
-	love.graphics.setColor(0, 1, 0, 0.5)
-	for j, w in ipairs(self.tracers.right) do
-		w:debugDraw()
-	end
+    love.graphics.setColor(0, 1, 0, 0.5)
+    for j, w in ipairs(self.tracers.right) do
+        w:debugDraw()
+    end
 
-	love.graphics.setColor(0, 0, 1, 0.5)
-	for j, w in ipairs(self.tracers.left) do
-		w:debugDraw()
-	end
+    love.graphics.setColor(0, 0, 1, 0.5)
+    for j, w in ipairs(self.tracers.left) do
+        w:debugDraw()
+    end
 
-	love.graphics.setColor(1, 0, 1, 0.5)
-	for j, w in ipairs(self.tracers.down) do
-		w:debugDraw()
-	end
+    love.graphics.setColor(1, 0, 1, 0.5)
+    for j, w in ipairs(self.tracers.down) do
+        w:debugDraw()
+    end
 
-	love.graphics.setColor(1, 1, 0, 0.5)
-	for j, w in ipairs(self.tracers.up) do
-		w:debugDraw()
-	end
+    love.graphics.setColor(1, 1, 0, 0.5)
+    for j, w in ipairs(self.tracers.up) do
+        w:debugDraw()
+    end
 
-	love.graphics.setColor(1, 1, 1)
+    love.graphics.setColor(1, 1, 1)
 end
 
 function PhysObj:standingOnDebugDraw()
-	love.graphics.print(tostring(self.standingOn), self.x+self.width, self.y)
+    love.graphics.print(tostring(self.standingOn), self.x + self.width, self.y)
 end
 
-function PhysObj:leftCollision() end
-function PhysObj:rightCollision() end
-function PhysObj:bottomCollision() end
-function PhysObj:topCollision() end
-function PhysObj:startFall() end
+function PhysObj:leftCollision()
+end
+function PhysObj:rightCollision()
+end
+function PhysObj:bottomCollision()
+end
+function PhysObj:topCollision()
+end
+function PhysObj:startFall()
+end
 
 return PhysObj
--- lib/Physics3/Tracer.lua
+++ lib/Physics3/Tracer.lua
@@ -1,93 +1,93 @@
 local Tracer = class("Physics3.Tracer") -- The cavalry is here
 
 function Tracer:initialize(physObj, x, y, vector)
-	self.physObj = physObj
-	self.x = x
-	self.y = y
-	self.vector = vector
-	self.vectorNormalized = self.vector:normalized()
-	self.len = self.vector:len()
-	self.tracedLength = 0
+    self.physObj = physObj
+    self.x = x
+    self.y = y
+    self.vector = vector
+    self.vectorNormalized = self.vector:normalized()
+    self.len = self.vector:len()
+    self.tracedLength = 0
 
-	self:cacheCoordinates()
+    self:cacheCoordinates()
 end
 
 function Tracer:cacheCoordinates()
-	self.coordinateCache = {}
+    self.coordinateCache = {}
 
-	for i = 1, self.len do
-		local x = self.x + self.vectorNormalized.x*i
-		local y = self.y + self.vectorNormalized.y*i
+    for i = 1, self.len do
+        local x = self.x + self.vectorNormalized.x * i
+        local y = self.y + self.vectorNormalized.y * i
 
-		self.coordinateCache[i] = {x, y}
-	end
+        self.coordinateCache[i] = {x, y}
+    end
 end
 
 function Tracer:trace()
-	-- Extra safety: if the physics object was destroyed, don't trace
-	if not self.physObj.world then
-		self.tracedLength = 0
-		return
-	end
+    -- Extra safety: if the physics object was destroyed, don't trace
+    if not self.physObj.world then
+        self.tracedLength = 0
+        return
+    end
 
-	local i = 1
+    local i = 1
 
-	while i <= self.len do
-		local x = self.coordinateCache[i][1] + self.physObj.x
-		local y = self.coordinateCache[i][2] + self.physObj.y
+    while i <= self.len do
+        local x = self.coordinateCache[i][1] + self.physObj.x
+        local y = self.coordinateCache[i][2] + self.physObj.y
 
-		local xRounded, yRounded
+        local xRounded, yRounded
 
-		if self.vector.y < 0 then -- don't ask me why
-			yRounded = math.ceil(y)
-		else
-			yRounded = math.floor(y)
-		end
+        if self.vector.y < 0 then -- don't ask me why
+            yRounded = math.ceil(y)
+        else
+            yRounded = math.floor(y)
+        end
 
-		if self.vector.x < 0 then
-			xRounded = math.ceil(x)
-		else
-			xRounded = math.floor(x)
-		end
+        if self.vector.x < 0 then
+            xRounded = math.ceil(x)
+        else
+            xRounded = math.floor(x)
+        end
 
-		local col = self.physObj.world:checkCollision(xRounded, yRounded, self.physObj, self.vectorNormalized)
-		if col then
-			self.tracedLength = i
-			return xRounded, yRounded, col
-		end
+        local col = self.physObj.world:checkCollision(xRounded, yRounded, self.physObj, self.vectorNormalized)
+        if col then
+            self.tracedLength = i
+            return xRounded, yRounded, col
+        end
 
-		i = i + 1
-	end
-	self.tracedLength = self.len
+        i = i + 1
+    end
+    self.tracedLength = self.len
 end
 
 function Tracer:debugDraw()
-	local angle = self.vector:angleTo()
+    local angle = self.vector:angleTo()
 
-	local x = self.x
-	local y = self.y
+    local x = self.x
+    local y = self.y
 
-	x = x + self.physObj.x
-	y = y + self.physObj.y
+    x = x + self.physObj.x
+    y = y + self.physObj.y
 
-	x = math.floor(x)
-	y = math.floor(y)
+    x = math.floor(x)
+    y = math.floor(y)
 
-	love.graphics.push()
-	love.graphics.translate(x+.5, y+.5)
-	love.graphics.rotate(angle)
+    love.graphics.push()
+    love.graphics.translate(x + .5, y + .5)
+    love.graphics.rotate(angle)
 
-	love.graphics.rectangle("fill", -.5, -.5, self.len, 1)
-	if VAR("debug").tracerDebug then
-		local r, g, b, a = love.graphics.getColor()
+    love.graphics.rectangle("fill", -.5, -.5, self.len, 1)
+    if VAR("debug").tracerDebug then
+        local r, g, b, a = love.graphics.getColor()
 
-		love.graphics.setColor(1, 0, 0)
-		love.graphics.rectangle("fill", -.5, -.5, self.tracedLength, 1)
+        love.graphics.setColor(1, 0, 0)
+        love.graphics.rectangle("fill", -.5, -.5, self.tracedLength, 1)
 
-		love.graphics.setColor(r, g, b, a)
-	end
+        love.graphics.setColor(r, g, b, a)
+    end
 
-	love.graphics.pop()
+    love.graphics.pop()
 end
 
 return Tracer
