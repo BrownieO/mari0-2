fbbb76c288ba8225c932328122ab1057ba2f7c25
BrownieO <187204825+BrownieO@users.noreply.github.com>
2025-11-21 22:37:59 -0300

    Make star actually kill

--- components/misc/isHurtByStar.lua
+++ components/misc/isHurtByStar.lua
@@ -1,19 +1,19 @@
 local Component = require "class.Component"
 local isHurtByStar = class("misc.isHurtByStar", Component)
 
-function isHurtByStar:rightCollision(dt, actorEvent, obj2)
+function isHurtByStar:rightContact(dt, actorEvent, obj2)
     self:resolve("left", obj2)
 end
 
-function isHurtByStar:leftCollision(dt, actorEvent, obj2)
+function isHurtByStar:leftContact(dt, actorEvent, obj2)
     self:resolve("right", obj2)
 end
 
-function isHurtByStar:topCollision(dt, actorEvent, obj2)
+function isHurtByStar:topContact(dt, actorEvent, obj2)
     self:resolve("bottom", obj2)
 end
 
-function isHurtByStar:bottomCollision(dt, actorEvent, obj2)
+function isHurtByStar:bottomContact(dt, actorEvent, obj2)
     self:resolve("top", obj2)
 end
 
