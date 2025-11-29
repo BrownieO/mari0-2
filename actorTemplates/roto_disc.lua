5d31bde6579fe841761f0f78e5a99d2cac8bed9b
BrownieO <187204825+BrownieO@users.noreply.github.com>
2025-11-29 09:24:43 -0300

    Finish roto-disc port

--- actorTemplates/roto_disc.lua
+++ actorTemplates/roto_disc.lua
@@ -17,6 +17,7 @@ return {
         ["animation.frames"] = {
             frames = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
         },
+		["movement.circle"] = {},
 		["misc.hurtsByContact"] = {
 			left = true,
 			right = true,
