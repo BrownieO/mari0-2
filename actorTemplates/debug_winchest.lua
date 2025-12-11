3a726f5b1bf00b5d95baeabb914e702b312742f2
BrownieO <187204825+BrownieO@users.noreply.github.com>
2025-12-11 04:01:16 -0300

    Add debug win chest actor

---
+++ actorTemplates/debug_winchest.lua
@@ -0,0 +1,21 @@
+return {
+    width = 16,
+    height = 16,
+
+    img = "img/actors/chest_small.png",
+    quadWidth = 16,
+    quadHeight = 16,
+    centerX = 8,
+    centerY = 8,
+	
+    collisionGroup = 16,
+    noncollide = 4,
+	
+    components = {
+        ["animation.frames"] = {
+            frames = {1}
+        },
+        ["misc.unrotate"] = {},
+		["debug.winOnContact"] = {},
+    }
+}
