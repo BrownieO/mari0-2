5a5eb35b48aab048c678843ce522cbb278141c71
BrownieO <187204825+BrownieO@users.noreply.github.com>
2025-12-11 04:01:16 -0300

    Add debug win chest actor

---
+++ components/debug/winOnContact.lua
@@ -0,0 +1,31 @@
+local Component = require "class.Component"
+local winOnContact = class("misc.winOnContact", Component)
+
+function winOnContact:initialize(actor, args)
+    Component.initialize(self, actor, args)
+end
+
+function winOnContact:leftContact(dt, actorEvent, obj2)
+    self:resolve("right", obj2)
+end
+
+function winOnContact:rightContact(dt, actorEvent, obj2)
+    self:resolve("left", obj2)
+end
+
+function winOnContact:topContact(dt, actorEvent, obj2)
+    self:resolve("bottom", obj2)
+end
+
+function winOnContact:bottomContact(dt, actorEvent, obj2)
+    self:resolve("top", obj2)
+end
+
+function winOnContact:resolve(dir, obj2)
+    local collectsCoins = obj2:hasComponent("misc.collectsCoins")
+    if collectsCoins then
+		self.actor:destroy()
+    end
+end
+
+return winOnContact
