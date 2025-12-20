1bf83da3ad1a43bc52c6aca2784c9c673c9f80e9
BrownieO <187204825+BrownieO@users.noreply.github.com>
2025-12-20 03:54:59 -0300

    Start custom note block force implementation

--- components/misc/bounceOnBlocks.lua
+++ components/misc/bounceOnBlocks.lua
@@ -9,15 +9,25 @@ function bounceOnBlocks:bottomCollision(dt, actorEvent, obj2)
 	if obj2.tile.props.noteblock then
 		self.actor.world:bounceOnBlock(obj2, self.actor)
 		
-		self.actor.speed[2] = -getRequiredSpeed(VAR("enemyBounceHeight"))
+		if obj2.tile.props.noteblockForceY then
+			self.actor.speed[2] = -getRequiredSpeed(obj2.tile.props.noteblockForceY)
+		else
+			self.actor.speed[2] = -getRequiredSpeed(VAR("enemyBounceHeight"))
+		end
 
 		actorEvent:bind("after", function(actor)
 			actor:switchState("falling") -- smb3.movement would love to set us to idle, but we can't have that
 		end)
 		
 		if controls3.cmdDown("jump") then
+			if obj2.tile.props.noteblockForceX then
+				self.actor.speed[1] = obj2.tile.props.noteblockForceX*2*3
+			end
 			playSound("note-block-long")
 		else
+			if obj2.tile.props.noteblockForceX then
+				self.actor.speed[1] = obj2.tile.props.noteblockForceX*3
+			end
 			playSound("note-block")
 		end
 	end
