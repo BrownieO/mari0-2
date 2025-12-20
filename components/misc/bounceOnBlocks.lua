94632d770a0b5babdcd5f95f2a7ce25666db87bf
BrownieO <187204825+BrownieO@users.noreply.github.com>
2025-12-20 04:34:32 -0300

    Noteblock crash hotfix

--- components/misc/bounceOnBlocks.lua
+++ components/misc/bounceOnBlocks.lua
@@ -6,29 +6,31 @@ bounceOnBlocks.argList = {
 }
 
 function bounceOnBlocks:bottomCollision(dt, actorEvent, obj2)
-	if obj2.tile.props.noteblock then
-		self.actor.world:bounceOnBlock(obj2, self.actor)
-		
-		if obj2.tile.props.noteblockForceY then
-			self.actor.speed[2] = -getRequiredSpeed(obj2.tile.props.noteblockForceY)
-		else
-			self.actor.speed[2] = -getRequiredSpeed(VAR("enemyBounceHeight"))
-		end
+	if obj.tile then
+		if obj2.tile.props.noteblock then
+			self.actor.world:bounceOnBlock(obj2, self.actor)
+			
+			if obj2.tile.props.noteblockForceY then
+				self.actor.speed[2] = -getRequiredSpeed(obj2.tile.props.noteblockForceY)
+			else
+				self.actor.speed[2] = -getRequiredSpeed(VAR("enemyBounceHeight"))
+			end
 
-		actorEvent:bind("after", function(actor)
-			actor:switchState("falling") -- smb3.movement would love to set us to idle, but we can't have that
-		end)
-		
-		if controls3.cmdDown("jump") then
-			if obj2.tile.props.noteblockForceX then
-				self.actor.speed[1] = obj2.tile.props.noteblockForceX*2*3
+			actorEvent:bind("after", function(actor)
+				actor:switchState("falling") -- smb3.movement would love to set us to idle, but we can't have that
+			end)
+			
+			if controls3.cmdDown("jump") then
+				if obj2.tile.props.noteblockForceX then
+					self.actor.speed[1] = obj2.tile.props.noteblockForceX*2*3
+				end
+				playSound("note-block-long")
+			else
+				if obj2.tile.props.noteblockForceX then
+					self.actor.speed[1] = obj2.tile.props.noteblockForceX*3
+				end
+				playSound("note-block")
 			end
-			playSound("note-block-long")
-		else
-			if obj2.tile.props.noteblockForceX then
-				self.actor.speed[1] = obj2.tile.props.noteblockForceX*3
-			end
-			playSound("note-block")
 		end
 	end
 end
