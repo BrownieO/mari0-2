7a78477a5c0817f8a10611201ab90199dd2ffb5e
BrownieO <187204825+BrownieO@users.noreply.github.com>
2025-12-27 16:23:57 -0300

    Improve stomp component

--- components/misc/stomps.lua
+++ components/misc/stomps.lua
@@ -6,19 +6,21 @@ stomps.argList = {
 }
 
 function stomps:bottomContact(dt, actorEvent, obj2)
-	if obj2:hasComponent("misc.stompable") and (self.actor.cache.speed[2] > 0 or (not self.actor.starred and not self.actor.iFramed)) then
-        -- self.actor.y = obj2.y-self.actor.height
-        self.actor.speed[2] = -getRequiredSpeed(VAR("enemyBounceHeight"))
+	if obj2:hasComponent("misc.stompable") and not self.actor.starred then
+		if self.actor.cache.speed[2] > 0 and not self.actor.iFramed then
+			-- self.actor.y = obj2.y-self.actor.height
+			self.actor.speed[2] = -getRequiredSpeed(VAR("enemyBounceHeight"))
 
-        actorEvent:bind("after", function(actor)
-            actor:switchState("falling") -- smb3.movement would love to set us to idle, but we can't have that
-        end)
+			actorEvent:bind("after", function(actor)
+				actor:switchState("falling") -- smb3.movement would love to set us to idle, but we can't have that
+			end)
 
-        actorEvent.returns = true
+			actorEvent.returns = true
 
-        obj2:event("getStomped")
-        self.actor:event("stomp")
-    end
+			obj2:event("getStomped")
+			self.actor:event("stomp")
+		end
+	end
 end
 
 return stomps
