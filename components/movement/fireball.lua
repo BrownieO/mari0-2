0f61f8529ba3b04e7316e64825743a87a3361ff4
BrownieO <187204825+BrownieO@users.noreply.github.com>
2026-01-10 04:48:11 -0300

    'Fix' bug of fireball collisions

--- components/movement/fireball.lua
+++ components/movement/fireball.lua
@@ -17,9 +17,16 @@ function fireball:bottomCollision(dt, actorEvent, obj2)
     self:resolve("top", obj2)
 end
 
+function fireball:shouldCollide(a, b)
+	return bit.band(a.collisionGroup, b.collisionMask) ~= 0
+	and bit.band(b.collisionGroup, a.collisionMask) ~= 0
+end
+
 function fireball:resolve(dir, obj2)
 	if dir == "top" then
-		self.actor.speed[2] = -160
+		if self:shouldCollide(obj2, self.actor) then
+			self.actor.speed[2] = -140
+		end
 	else
 		self.actor:destroy()
 	end
