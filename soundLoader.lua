aeeb00c7d15154f1fa7057f29f8328e6d4baba9d
BrownieO <187204825+BrownieO@users.noreply.github.com>
2026-01-23 08:23:54 -0300

    Add echo presets

--- soundLoader.lua
+++ soundLoader.lua
@@ -1,4 +1,24 @@
 sounds = {}
+local echoSmw = {
+	type = "echo",
+	volume = 0.1,
+	delay = 0,
+	tapdelay = 0.0675,
+	damping = 0.25,
+	feedback = 0.85,
+	spread = 0
+}
+local echoBasic = {
+	type = "echo",
+	volume = 0.5,
+	delay = 0,
+	tapdelay = 0.1,
+	damping = 0,
+	feedback = 0.5,
+	spread = 0
+}
+
+success = love.audio.setEffect("echo", echoBasic)
 
 local toLoad = recursiveEnumerate("sound")
 
@@ -6,6 +26,7 @@ for _, path in ipairs(toLoad) do
 	name = path:sub(7) -- remove folder name ("sound/"). lol hardcode
 	name = name:gsub("%.[^/\\%.]+$", "") -- remove extension
 	sounds[name] = love.audio.newSource(path, "static")
+	--sounds[name]:setEffect("echo")
 end
 
 local toLoad = recursiveEnumerate("music")
