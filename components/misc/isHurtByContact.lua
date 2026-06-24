1cbc89a84cc7f73d32f5b3eab04c3156fc40fc4e
BrownieO <187204825+BrownieO@users.noreply.github.com>
2026-06-23 23:10:41 -0400

    Add FIXME comment regarding #103

--- components/misc/isHurtByContact.lua
+++ components/misc/isHurtByContact.lua
@@ -22,7 +22,9 @@ function isHurtByContact:resolve(dir, obj2)
     local hurtsByContactComponent = obj2:hasComponent("misc.hurtsByContact")
     if hurtsByContactComponent and hurtsByContactComponent[dir] then
         if not hurtsByContactComponent.onlyWhenMoving or obj2.cache.speed[1] ~= 0 then
-            if not hurtsByContactComponent.group or bit.band(self.actor.collisionGroup, hurtsByContactComponent.group) ~= 0 then
+            if not hurtsByContactComponent.group
+			or bit.band(self.actor.collisionGroup, hurtsByContactComponent.group) ~= 0 then
+			-- FIXME: make hurt filter independent of collision masks
 				self.actor:event("getHurt", dt)
 				if hurtsByContactComponent.kamikaze then
 					obj2:destroy()
