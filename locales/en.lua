ee266188c5ae8eaf227e1476241010bbbd8da304
BrownieO <187204825+BrownieO@users.noreply.github.com>
2026-07-14 11:16:55 -0400

    Improve assertion messages somewhat

--- locales/en.lua
+++ locales/en.lua
@@ -1,84 +1,91 @@
 return {
-	startMsg = "english localization working",
-	
-	mainMenu = {
-		onePlayerGame = "1 player game",
-		selectMappack = "Select mappack",
-		levelEditor = "Level editor",
-		options = "Options"
-	},
-	
-	options = {
-		options = "OPTIONS",
-		language = "🌐 Language",
-		resetMappacks = "Reset included mappacks"
-	},
-	
-	editor = {
-		open = "Open",
-		save = "Save",
-		saveAs = "Save as",
-		play = "Play",
-		_exit = "Exit",
-		
-		file = "File",
-		window = "Window",
-		view = "View",
-	
-		_debug = "Debug",
-		layers = "Layers",
-		mapOptions = "Map options",
-		minimap = "Minimap",
-		stamps = "Stamps",
-		tiles = "Tiles",
-		entities = "Entities",
-		
-		freeCamera = "Free camera",
-		drawGrid = "Draw grid",
-		hideUi = "Hide UI",
-		
-		levelName = "Level name:",
-		back = "< Back",
-	},
-	
-	state = "state",
-	powerUp = "power-up",
-	forms = {
-		small = "small",
-		
-		big = "big",
-		fire = "fire",
-		raccoon = "raccoon",
-		frog = "frog",
-		tanooki = "tanooki",
-		hammer = "hammer",
-		balloon = "balloon",
+    startMsg = "english localization working",
 
-		invincible = "invincible",
-		metal = "metal",
-	},
-	
-	window = {
-		openFile = "Open file",
-		saveFile = "Save file",
-		playtest = "Playtest",
-		editor = "Editor",
-		saveFirst = "Save the changes first!",
-		saveChanges = "Save changes before quitting?",
-		
-		ok = "OK",
-		cancel = "Cancel",
-		yes = "Yes",
-		no = "No"		
-	},
-	
-	assertions = {
-		tilemapNonexistent = "Map tried to tell me that it uses the tilemap \"%s\" but we all know that's bullshit because that tilemap's settings.lua doesn't exist.",
-		paddingBigger = "StampMap \"%s\" from the TileMap \"%s\" had a padding[%s] bigger than its own size. I hope I don't have to explain how nonsensical that is.",
-		tileLoadError = "Couldn't load real tile at x=%s, y=%s for requested lookup \"%s\". This may mean that the map is corrupted.",
-		tilemapNotPassed = "The map file has no tilemap with the number %s",
-		elementToItself = "You can't add an element to itself. That's stupid.",
-		noCrosshair = "Actor tried to fire a portal without having a crosshair.",
-		faultyQuad = [[The state "%s" on actorTemplate %s has a faulty quad. (attempted frame was "%s)]]
-	}
+    mainMenu = {
+        onePlayerGame = "1 player game",
+        selectMappack = "Select mappack",
+        levelEditor = "Level editor",
+        options = "Options",
+    },
+
+    options = {
+        options = "OPTIONS",
+        language = "🌐 Language",
+        resetMappacks = "Reset included mappacks",
+    },
+
+    editor = {
+        open = "Open",
+        save = "Save",
+        saveAs = "Save as",
+        play = "Play",
+        _exit = "Exit",
+
+        file = "File",
+        window = "Window",
+        view = "View",
+
+        _debug = "Debug",
+        layers = "Layers",
+        mapOptions = "Map options",
+        minimap = "Minimap",
+        stamps = "Stamps",
+        tiles = "Tiles",
+        entities = "Entities",
+
+        freeCamera = "Free camera",
+        drawGrid = "Draw grid",
+        hideUi = "Hide UI",
+
+        levelName = "Level name:",
+        back = "< Back",
+    },
+
+    state = "state",
+    powerUp = "power-up",
+    forms = {
+        small = "small",
+
+        big = "big",
+        fire = "fire",
+        raccoon = "raccoon",
+        frog = "frog",
+        tanooki = "tanooki",
+        hammer = "hammer",
+        balloon = "balloon",
+
+        invincible = "invincible",
+        metal = "metal",
+    },
+
+    window = {
+        openFile = "Open file",
+        saveFile = "Save file",
+        playtest = "Playtest",
+        editor = "Editor",
+        saveFirst = "Save the changes first!",
+        saveChanges = "Save changes before quitting?",
+
+        ok = "OK",
+        cancel = "Cancel",
+        yes = "Yes",
+        no = "No",
+    },
+
+    assertions = {
+        -- Level
+        tilemapNonexistent = 'The tilemap "%s" requested by the map doesn\'t exist.',
+        tileLoadError = 'Couldn\'t load real tile at x=%s, y=%s for requested lookup "%s". This may mean that the map is corrupted.',
+        tilemapNotPassed = "The map file requests a tileset numbered %s, but it also needs to have its name inside.",
+
+        -- Tileset
+        paddingBigger = 'StampMap "%s" from the TileMap "%s" had a padding[%s] bigger than its own size. Please decrease it.',
+
+        -- Gui3
+        elementToItself = "You can't add an element to itself. That's stupid.",
+
+        -- Components
+        faultyQuad = [[The state "%s" on actorTemplate %s has a faulty quad. (attempted frame was "%s)]],
+        noCrosshair = "Actor tried to fire a portal without having a crosshair.",
+    },
 }
--- locales/es.lua
+++ locales/es.lua
@@ -1,84 +1,91 @@
 return {
-	startMsg = "localización al español funcionando",
-	
-	mainMenu = {
-		onePlayerGame = "Juego de 1 jugador",
-		selectMappack = "Seleccionar juego",
-		levelEditor = "Editor de niveles",
-		options = "Opciones"
-	},
-	
-	options = {
-		options = "OPCIONES",
-		language = "🌐 Idioma",
-		resetMappacks = "Reestablecer juegos incluidos"
-	},
-	
-	editor = {
-		open = "Abrir",
-		save = "Guardar",
-		saveAs = "Guardar como",
-		play = "Jugar",
-		_exit = "Salir",
-		
-		file = "Archivo",
-		window = "Ventana",
-		view = "Vista",
-	
-		_debug = "Depuración",
-		layers = "Capas",
-		mapOptions = "Opciones del nivel",
-		minimap = "Minimapa",
-		stamps = "Estampas",
-		tiles = "Tiles",
-		entities = "Entidades",
-		
-		freeCamera = "Cámara libre",
-		drawGrid = "Cuadrícula",
-		hideUi = "Ocultar interfaz",
-		
-		levelName = "Nombre:",
-		back = "< Volver",
-	},
-	
-	state = "estado",
-	powerUp = "power-up",
-	forms = {
-		small = "pequeño",
-		
-		big = "grande",
-		fire = "fuego",
-		raccoon = "mapache",
-		frog = "rana",
-		tanooki = "tanuki",
-		hammer = "martillo",
-		balloon = "globo",
+    startMsg = "localización al español funcionando",
 
-		invincible = "invencible",
-		metal = "metal",
-	},
-	
-	window = {
-		openFile = "Abrir archivo",
-		saveFile = "Guardar archivo",
-		playtest = "Prueba de juego",
-		editor = "Editor",
-		saveFirst = "Se deben guardar los cambios antes de continuar.",
-		saveChanges = "¿Guardar cambios antes de salir?",
-		
-		ok = "Aceptar",
-		cancel = "Cancelar",
-		yes = "Sí",
-		no = "No"		
-	},
-	
-	assertions = {
-		tilemapNonexistent = "El tileset \"%s\" no existe.",
-		paddingBigger = "El estampado \"%s\" del tileset \"%s\" tiene un margen [%s] más grande que su propio tamaño. Mala idea.",
-		tileLoadError = "No se pudo cargar el tile en x=%s, y=%s con ID \"%s\". El nivel puede estar corrupto.",
-		tilemapNotPassed = "El archivo del nivel no especifica ningún tileset con número %s",
-		elementToItself = "No se puede emparentar un elemento consigo mismo.",
-		noCrosshair = "Un actor intentó abrir un portal sin tener una retícula.",
-		faultyQuad = [[El estado "%s" en actorTemplate %s tiene un cuadro malo. (fotograma "%s")]]
-	}
+    mainMenu = {
+        onePlayerGame = "Juego de 1 jugador",
+        selectMappack = "Seleccionar juego",
+        levelEditor = "Editor de niveles",
+        options = "Opciones",
+    },
+
+    options = {
+        options = "OPCIONES",
+        language = "🌐 Idioma",
+        resetMappacks = "Reestablecer juegos incluidos",
+    },
+
+    editor = {
+        open = "Abrir",
+        save = "Guardar",
+        saveAs = "Guardar como",
+        play = "Jugar",
+        _exit = "Salir",
+
+        file = "Archivo",
+        window = "Ventana",
+        view = "Vista",
+
+        _debug = "Depuración",
+        layers = "Capas",
+        mapOptions = "Opciones del nivel",
+        minimap = "Minimapa",
+        stamps = "Estampas",
+        tiles = "Tiles",
+        entities = "Entidades",
+
+        freeCamera = "Cámara libre",
+        drawGrid = "Cuadrícula",
+        hideUi = "Ocultar interfaz",
+
+        levelName = "Nombre:",
+        back = "< Volver",
+    },
+
+    state = "estado",
+    powerUp = "power-up",
+    forms = {
+        small = "pequeño",
+
+        big = "grande",
+        fire = "fuego",
+        raccoon = "mapache",
+        frog = "rana",
+        tanooki = "tanuki",
+        hammer = "martillo",
+        balloon = "globo",
+
+        invincible = "invencible",
+        metal = "metal",
+    },
+
+    window = {
+        openFile = "Abrir archivo",
+        saveFile = "Guardar archivo",
+        playtest = "Prueba de juego",
+        editor = "Editor",
+        saveFirst = "Se deben guardar los cambios antes de continuar.",
+        saveChanges = "¿Guardar cambios antes de salir?",
+
+        ok = "Aceptar",
+        cancel = "Cancelar",
+        yes = "Sí",
+        no = "No",
+    },
+
+    assertions = {
+        -- Level
+        tilemapNonexistent = 'El tileset "%s" requerido por el mapa no existe.',
+        tileLoadError = 'No se pudo cargar el tile en x=%s, y=%s con ID "%s". El nivel puede estar corrupto.',
+        tilemapNotPassed = "El archivo del nivel solicita un tileset con número %s, pero falta escribir su nombre.",
+
+        -- Tileset
+        paddingBigger = 'El estampado "%s" del tileset "%s" tiene un margen [%s] más grande que su propio tamaño. Por favor achíquelo.',
+
+        -- Gui3
+        elementToItself = "No se puede emparentar un elemento consigo mismo.",
+
+        -- Components
+        faultyQuad = [[El estado "%s" en actorTemplate %s tiene un cuadro malo. (fotograma "%s")]],
+        noCrosshair = "Un actor intentó abrir un portal sin tener una retícula.",
+    },
 }
