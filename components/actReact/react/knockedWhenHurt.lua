edc7893a95b73339e695eb60ea675d47ace2938f
BrownieO <187204825+BrownieO@users.noreply.github.com>
2026-08-30 23:00:13 -0400

    Delete unused components

--- components/actReact/react/knockedWhenHurt.lua
+++
@@ -1,9 +0,0 @@
-local Component = require("class.Component")
-local knockedWhenHurt = class("actReact.react.knockedWhenHurt", Component)
-
-function knockedWhenHurt:getHurt()
-    playSound("knock")
-    self.actor:destroy()
-end
-
-return knockedWhenHurt
